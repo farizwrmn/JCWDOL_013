4d44a8c566e73f0e72cec99e172fc1b48feb4776
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-02-08 18:35:50 +0700

    menambahkan folder dan exercise5.js

---
+++ modul 5/exercise5.js
@@ -0,0 +1,4 @@
+// Write a function to get the lowest, average and highest value in array //
+// contoh : arr = [12, 5, 23, 18, 4, 45, 32] -> {lowest : 4, highest : 45, average: 19.8}
+
+function Arr() {}
