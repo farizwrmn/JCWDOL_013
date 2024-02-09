2c4887a7ad85622ccc3cadba3b137975211f9288
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-02-09 13:24:46 +0700

    update exercise5.js

--- modul 5/exercise5.js
+++ modul 5/exercise5.js
@@ -1,4 +1,20 @@
 // Write a function to get the lowest, average and highest value in array //
 // contoh : arr = [12, 5, 23, 18, 4, 45, 32] -> {lowest : 4, highest : 45, average: 19.8}
 
-function Arr() {}
+function Arr(max) {
+  let highest = Math.max(...max);
+  let num1 = Math.min(...max);
+  let avg = "";
+  let hasil = "";
+  for (let i = 0; i < max.length; i++) {
+    avg += max[i];
+  }
+  avg = avg / max.length;
+
+  return `lowest : ${hasil + num1}, highest : ${hasil + highest}, average : ${
+    hasil + Math.floor(avg)
+  }`;
+}
+
+console.log(Arr([12, 5, 23, 18, 4, 45, 32]));
+//----------------------------------------------------------------------------------------------//
