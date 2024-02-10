ec747f37e66db7c6a07d40a4c217a9443fb07ff9
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-02-10 13:14:42 +0700

    update exercise5.js

--- modul 5/exercise5.js
+++ modul 5/exercise5.js
@@ -83,4 +83,62 @@ console.log(b([1, 2, 3, 4, 5, 6]));
 // Contoh : maxSize = 5, given integers is 5, 10, 24, 3, 6, 7, 8
 // The function will return [5, 10, 24, 3, 6]
 
-function maxSize() {}
+function maxSize(limit, ...num) {
+  return num.slice(0, limit);
+}
+
+const limitArr = 5;
+console.log(maxSize(limitArr, 5, 10, 24, 3, 6, 7, 8));
+//------------------------------------------------------//
+
+// Write a function that will combine 2 given array into one array //
+// contoh : arr1 = [1, 2, 3], arr2 = [4, 5, 6] -> [1, 2, 3, 4, 5, 6]
+
+const comb = function (no, no1) {
+  return no.concat(no1);
+};
+
+const arr3 = [1, 2, 3];
+const arr4 = [4, 5, 6];
+
+console.log(comb(arr3, arr4));
+//-----------------------------------------------------//
+
+// Write a function to find duplicate values in array //
+// contoh : arr = [1, 2, 2, 2, 3, 3, 4, 5, 5] -> [2, 3, 5]
+
+const arr5 = [1, 2, 2, 2, 3, 3, 4, 5, 5];
+
+const dupl = function () {
+  let z = arr5.filter((item, index) => arr5.indexOf(item) !== index);
+  return z;
+};
+
+const duplElements = dupl(arr5);
+console.log(duplElements);
+//--------------------------------------------------------//
+
+// Write a function to find diferentiate between 2 arrays //
+// contoh : arr1 = [1, 2, 3, 4, 5], arr = [3, 4, 5, 6, 7] -> [1, 2, 6, 7]
+
+function dif(data, data1) {
+  let removed = [];
+  for (i of data) {
+    if (!removed.includes(i) && !data1.includes(i)) {
+      removed.push(i);
+      console.log(i);
+    }
+  }
+  for (i of data1) {
+    if (!removed.includes(i) && !data.includes(i)) {
+      removed.push(i);
+    }
+    console.log(i);
+  }
+  return removed;
+}
+const c = [1, 2, 3, 4, 5];
+const d = [3, 4, 5, 6, 7];
+
+console.log(dif(c, d));
+//--------------------------------------------------------//
