1b215077afa39f09b59e6d44f5955eb24364ef09
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-02-11 19:40:43 +0700

    update exercise5_1.js

--- modul 5/exercise5_1.js
+++ modul 5/exercise5_1.js
@@ -12,3 +12,15 @@ function a(primitive) {
 const arr = [1, [], undefined, {}, "string", {}, []];
 console.log(a(arr));
 //----------------------------------------------------//
+
+// Write a function to return the second smallest number //
+// contoh : numbers = [5, 3, 1, 7, 2, 6] -> 2
+
+function b(num) {
+  let result = "";
+  let order = num.sort();
+  return result + order[1];
+}
+
+const numbers = [5, 3, 1, 7, 2, 6];
+console.log(b(numbers));
